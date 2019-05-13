1541f9ec71c1cfdf017a61ffa14c0016cda051e6
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-05-13 13:30:15 +0800

    skip(1)的意思就是跳过查询到的第一条数据的意思

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/about', function(req,res){
 
 app.get('/products', async function(req,res){
     // 代替之前写死的数据 每次的从数据库查询数据都是异步操作所以使用await 那么这个函数必须加上async他俩是成对出现使用的。
-    const data = await Product.find().limit(2)
+    const data = await Product.find().skip(1).limit(2)
     res.send(data)
 })
 

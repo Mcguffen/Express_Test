db53888348eed4646d8bdf26b3292a15bab01db2
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-05-13 13:09:29 +0800

    引入mongoose并链接数据库

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express")
 
 const app = express()
 
+// 引入mongoose 他是链接操作数据库的驱动
+const mongoose = require('mongoose')
+// 链接数据库,第一个参数是要链接的数据库地址,它的好处就是即使没有这个express_test数据库他会帮你创建，有的话直接链接很方便，第二个参数必须填写，新版本要求。
+mongoose.connect('mongodb://localhost:27017/express_test',{
+    useNewUrlParser: true
+})
+
 // 引入cors来解决跨域问题,他相当于一个中间件。
 app.use(require('cors')())
 

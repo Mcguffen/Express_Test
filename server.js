ba8450fa00788f0d2f00cff99a8796283e4001cb
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-05-13 15:54:48 +0800

    完成修改接口功能

--- server.js
+++ server.js
@@ -13,19 +13,19 @@ mongoose.connect('mongodb://localhost:27017/express_test',{
 })
 // 定义数据模型 第一个参数是模型名字一般我们大写，第二个参数是数据的表结构，他的参数是哥对象来定义数据的属性。
 const Product = mongoose.model('Product', new mongoose.Schema({
-    titile: String,
+    title: String,
 
 }))
 // 测试插入三条条数据来测试
 // Product.insertMany([
 //     {
-//         titile: '产品1',
+//         title: '产品1',
 //     },
 //     {
-//         titile: '产品2',
+//         title: '产品2',
 //     },
 //     {
-//         titile: '产品3',
+//         title: '产品3',
 //     },
 // ])
 
@@ -85,13 +85,27 @@ app.post('/products', async function(req, res){
  // 删除数据 删除产品(单个)
 app.delete('/products/:id', async function(req,res){
     // 先找到要删除的产品
-    const product = await Product.findById(res.params.id)
+    const product = await Product.findById(req.params.id)
     // 然后执行删除操作
-    product.remove()
+    console.log(product)
+    await product.remove()
     // 我们删除的数据 返回给客户端空的数据没有意义 所以我们返回状态 删除成功的状态
     res.send({
         success: true
     })
+
+})
+
+// 修改数据 修改莫个具体的产品
+app.put('/products/:id', async function(req,res){
+    const product = await Product.findById(req.params.id)
+    // 然后执行修改操作 内存操作
+    product.title = req.body.title
+    // 存到数据库 异步操作
+    await product.save()
+    console.log(product)
+    res.send(product)
+    
 
 })
 app.listen(3000, () => {

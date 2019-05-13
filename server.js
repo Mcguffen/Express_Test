4223b7dee91b0eda0331915a351bd71f4711a348
Poetic Justice <mcguffen@hanshanyang.xyz>
2019-05-13 13:19:12 +0800

    将之前写死的数据换成从数据库查询获得。
    因为第一次数据库是没有数据的所以我们可以插入一条数据来测试，一般情况要写一个接口来插入。

--- server.js
+++ server.js
@@ -35,21 +35,9 @@ app.get('/about', function(req,res){
     )
 })
 
-app.get('/products', function(req,res){
-    res.send([
-        {
-            id: '1',
-            titile: '产品1'
-        },
-        {
-            id: '2',
-            titile: '产品2'
-        },
-        {
-            id: '3',
-            titile: '产品3'
-        },
-    ])
+app.get('/products', async function(req,res){
+    // 代替之前写死的数据 每次的从数据库查询数据都是异步操作所以使用await 那么这个函数必须加上async他俩是成对出现使用的。
+    res.send(await Product.find())
 })
 
 app.listen(3000, ()=> {
